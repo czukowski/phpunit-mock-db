7fd1fabd51334956d7078222dd44cce810632b21
Korney Czukowski <carbofos@seznam.cz>
2018-03-19 07:22:48 +0100

    Query invocation implementation

---
+++ classes/Invocation/QueryInvocation.php
@@ -0,0 +1,94 @@
+<?php
+namespace Cz\PHPUnit\MockDB\Invocation;
+
+use Cz\PHPUnit\MockDB\Invocation;
+
+/**
+ * QueryInvocation
+ * 
+ * @author   czukowski
+ * @license  MIT License
+ */
+class QueryInvocation implements Invocation
+{
+    /**
+     * @var  integer
+     */
+    private $affectedRows;
+    /**
+     * @var  mixed
+     */
+    private $lastInsertId;
+    /**
+     * @var  string
+     */
+    private $query;
+    /**
+     * @var  mixed
+     */
+    private $resultSet;
+
+    /**
+     * @param  string  $query
+     */
+    public function __construct(string $query)
+    {
+        $this->query = $query;
+    }
+
+    /**
+     * @return  string
+     */
+    public function getQuery(): string
+    {
+        return $this->query;
+    }
+
+    /**
+     * @return  integer
+     */
+    public function getAffectedRows()
+    {
+        return $this->affectedRows;
+    }
+
+    /**
+     * @param  integer  $count
+     */
+    public function setAffectedRows(int $count)
+    {
+        $this->affectedRows = $count;
+    }
+
+    /**
+     * @return  mixed
+     */
+    public function getLastInsertId()
+    {
+        return $this->lastInsertId;
+    }
+
+    /**
+     * @param  mixed  $value
+     */
+    public function setLastInsertId($value)
+    {
+        $this->lastInsertId = $value;
+    }
+
+    /**
+     * @return  array
+     */
+    public function getResultSet()
+    {
+        return $this->resultSet;
+    }
+
+    /**
+     * @param  mixed  $result
+     */
+    public function setResultSet($result)
+    {
+        $this->resultSet = $result;
+    }
+}
---
+++ tests/Invocation/QueryInvocationTest.php
@@ -0,0 +1,103 @@
+<?php
+namespace Cz\PHPUnit\MockDB\Invocation;
+
+use Cz\PHPUnit\MockDB\Testcase,
+    ArrayObject;
+
+/**
+ * QueryInvocationTest
+ * 
+ * @author   czukowski
+ * @license  MIT License
+ */
+class QueryInvocationTest extends Testcase
+{
+    /**
+     * @dataProvider  provideGetQuery
+     */
+    public function testGetQuery($query)
+    {
+        $object = $this->createObject($query);
+        $this->assertSame($query, $object->getQuery());
+    }
+
+    public function provideGetQuery()
+    {
+        return [
+            ['SELECT * FROM `t`'],
+        ];
+    }
+
+    /**
+     * @dataProvider  provideAffectedRows
+     */
+    public function testAffectedRows($query, $value)
+    {
+        $object = $this->createObject($query);
+        $object->setAffectedRows($value);
+        $this->assertSame($value, $object->getAffectedRows());
+    }
+
+    public function provideAffectedRows()
+    {
+        return [
+            ['UPDATE `t` SET `a` = 1 WHERE `b` = 2', 100],
+        ];
+    }
+
+    /**
+     * @dataProvider  provideLastInsertId
+     */
+    public function testLastInsertId($query, $value)
+    {
+        $object = $this->createObject($query);
+        $object->setLastInsertId($value);
+        $this->assertSame($value, $object->getLastInsertId());
+    }
+
+    public function provideLastInsertId()
+    {
+        return [
+            ['INSERT INTO `t` (`a`, `b`) VALUES (1, 2)', 505],
+        ];
+    }
+
+    /**
+     * @dataProvider  provideResultSet
+     */
+    public function testResultSet($query, $results)
+    {
+        $object = $this->createObject($query);
+        $object->setResultSet($results);
+        $this->assertSame($results, $object->getResultSet());
+    }
+
+    public function provideResultSet()
+    {
+        return [
+            [
+                'SELECT * FROM `t1`',
+                [],
+            ],
+            [
+                'SELECT * FROM `t2`',
+                [
+                    ['id' => 1, 'name' => 'foo'],
+                    ['id' => 2, 'name' => 'bar'],
+                ],
+            ],
+            [
+                'SELECT * FROM `t3`',
+                new ArrayObject([
+                    ['id' => 1, 'name' => 'foo'],
+                    ['id' => 2, 'name' => 'bar'],
+                ]),
+            ],
+        ];
+    }
+
+    private function createObject($query)
+    {
+        return new QueryInvocation($query);
+    }
+}
